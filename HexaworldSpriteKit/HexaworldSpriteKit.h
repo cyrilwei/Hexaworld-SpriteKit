c4155772b59b3249d6faae31f1ec1ef962e1df2d
Cyril Wei <teakscn@gmail.com>
2014-07-06 17:42:28 +0800

    [cyril] Initial commit; created project.

---
+++ HexaworldSpriteKit/HexaworldSpriteKit.h
@@ -0,0 +1,19 @@
+//
+//  HexaworldSpriteKit.h
+//  HexaworldSpriteKit
+//
+//  Created by Cyril Wei on 7/6/14.
+//  Copyright (c) 2014 Cyril Wei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for HexaworldSpriteKit.
+FOUNDATION_EXPORT double HexaworldSpriteKitVersionNumber;
+
+//! Project version string for HexaworldSpriteKit.
+FOUNDATION_EXPORT const unsigned char HexaworldSpriteKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <HexaworldSpriteKit/PublicHeader.h>
+
+
